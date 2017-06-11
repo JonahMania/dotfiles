04a98dd23aae60d7e5035d55ae60feae8976f6b8
jonahmania <jmania999@gmail.com>
2017-06-10 20:31:05 -0400

    Update power and player scripts

--- scripts/.config/scripts/bar/player.sh
+++ scripts/.config/scripts/bar/player.sh
@@ -1,44 +1,6 @@
-INFO=$(mocp -i)
-STATE=$( echo "${INFO}" | grep "State:" | sed "s/State: //g" )
+#!/bin/bash
+album=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "album"|egrep -v "album"|egrep -v "array"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
+title=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
 
-# If nothing is player dont display anything
-if [ "${STATE}" = "STOP" ]; then
-    exit
-fi 
-
-case "${1}" in
-    "title" )
-        ARTIST=$( echo "${INFO}" | grep "Artist:" | sed "s/Artist: //g" )
-        SONG=$( echo "${INFO}" | grep "SongTitle:" | sed "s/SongTitle: //g" )
-        if [ "${BLOCK_BUTTON}" != "" ]; then 
-            mocp --toggle-pause
-        fi
-        if [ "${STATE}" != "PLAY" ]; then
-            echo " ${ARTIST} - ${SONG} " 
-        else
-            echo " ${ARTIST} - ${SONG} "  
-        fi;;
-        # Remove if and uncomment to get rid of play pause buttone in title 
-        #echo "  ${ARTIST} - ${SONG}";;
-    "play" )
-        if [ "${BLOCK_BUTTON}" != "" ]; then 
-            mocp --toggle-pause
-        fi
-        if [ "${STATE}" != "PLAY" ]; then
-            echo 
-        else
-            echo    
-        fi;;
-    "previous" )   
-        if [ "${BLOCK_BUTTON}" != "" ]; then 
-            mocp --previous
-        fi
-        echo ;;
-    "next" )   
-        if [ "${BLOCK_BUTTON}" != "" ]; then 
-            mocp --next
-        fi
-        echo ;;
-esac
-
-
+echo " [${artist}] ${title}"
