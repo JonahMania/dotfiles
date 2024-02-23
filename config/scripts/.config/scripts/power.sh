5f3ff3777ae542f2a0cbab75abfaae1f0967869b
Jonah Mania <jonahmania@gmail.com>
2024-02-22 20:20:04 -0800

    Update

--- config/scripts/.config/scripts/power.sh
+++ config/scripts/.config/scripts/power.sh
@@ -19,6 +19,8 @@ function has_battery() {
 function battery_level() {
     battery_level=0
     battery_count=0
+    battery_status=""
+
     for power_supply in $POWER_SUPPLY_DIRECTORY/*
     do
         if [ ! -f $power_supply/type ]; then
@@ -28,23 +30,29 @@ function battery_level() {
         if grep $power_supply/type -e "Battery" -q; then
             battery_level=$(($battery_level + $(cat $power_supply/capacity)))
             battery_count=$(($battery_count + 1))
+            battery_status=$(cat $power_supply/status)
         fi
     done
 
     if (($battery_count > 0)); then
         battery_level=$(($battery_level / $battery_count))
+        icon="❤"
+        if [ "$battery_status" = "Charging" ]; then
+            icon="⋆"
+        fi
 
         if (($battery_level > 80)); then
-            echo "%{T2}❤❤❤❤%{T-}"
+            echo "%{T2}${icon}${icon}${icon}${icon}%{T-}"
         elif (($battery_level > 60)); then
-            echo "%{T2}❤❤❤%{F${BAR_POWER_DEPLETED}}❤%{F-}%{T-}"
+            echo "%{T2}${icon}${icon}${icon}%{F${BAR_POWER_DEPLETED}}${icon}%{F-}%{T-}"
         elif (($battery_level > 40)); then
-            echo "%{T2}❤❤%{F${BAR_POWER_DEPLETED}}❤❤%{F-}%{T-}"
+            echo "%{T2}${icon}${icon}%{F${BAR_POWER_DEPLETED}}${icon}${icon}%{F-}%{T-}"
         elif (($battery_level > 20)); then
-            echo "%{T2}❤%{F${BAR_POWER_DEPLETED}}❤❤❤%{F-}%{T-}"
+            echo "%{T2}${icon}%{F${BAR_POWER_DEPLETED}}${icon}${icon}${icon}%{F-}%{T-}"
         else
-            echo "%{T2}%{F${BAR_POWER_DEPLETED}}❤❤❤❤%{F-}%{T-}"
+            echo "%{T2}%{F${BAR_POWER_DEPLETED}}${icon}${icon}${icon}${icon}%{F-}%{T-}"
         fi
+
         exit 0
     fi
     exit 1
